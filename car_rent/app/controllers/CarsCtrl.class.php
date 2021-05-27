8afc71c3024bc174860e74e9ef5b8abd3ebb019c
ThugRaven <kamilo1715@wp.pl>
2021-05-27 23:23:29 +0200

    Reformat functions

--- car_rent/app/controllers/CarsCtrl.class.php
+++ car_rent/app/controllers/CarsCtrl.class.php
@@ -37,65 +37,12 @@ class CarsCtrl {
         ];
     }
 
-    public function getParams() {
+    public function processCars() {
         $this->form->model = ParamUtils::getFromRequest('model');
         $this->form->brand = ParamUtils::getFromRequest('brand');
         $this->form->order = ParamUtils::getFromRequest('order');
         $this->form->type = ParamUtils::getFromRequest('transmission_type');
         $this->form->drive = ParamUtils::getFromRequest('drive');
-    }
-
-    public function processCar() {
-        $this->form_rent->id_car = $this->v->validateFromCleanURL(1, [
-            'required' => true,
-        ]);
-
-        if (!App::getMessages()->isError()) {
-            $where['id_car'] = $this->form_rent->id_car;
-
-            $brand = ParamUtils::getFromCleanURL(2);
-            $model = ParamUtils::getFromCleanURL(3);
-
-            $this->form_rent->rent_start = (new \DateTime('now', new \DateTimeZone('Europe/Warsaw')))->format('Y-m-d\TH:i');
-            $this->form_rent->rent_end = (new \DateTime('+1 day', new \DateTimeZone('Europe/Warsaw')))->format('Y-m-d\TH:i');
-
-            try {
-                $this->records = App::getDB()->get('car', [
-                    '[><]car_price' => 'id_car_price'
-                        ], '*', $where);
-
-                if (empty($this->records)) {
-                    Utils::addErrorMessage('Brak pojazdu o podanym ID!');
-                    SessionUtils::storeMessages();
-                    return false;
-                }
-
-                $this->records['brand_url'] = trim($this->records['brand']);
-                $this->records['model_url'] = trim($this->records['model']);
-                $this->records['brand_url'] = strtolower($this->records['brand_url']);
-                $this->records['model_url'] = strtolower($this->records['model_url']);
-                $this->records['brand_url'] = preg_replace('/\s+/', '-', $this->records['brand_url']);
-                $this->records['model_url'] = preg_replace('/\s+/', '-', $this->records['model_url']);
-
-                if ($brand !== $this->records['brand_url'] || $model !== $this->records['model_url']) {
-                    App::getRouter()->redirectTo("car/{$this->form_rent->id_car}/{$this->records['brand_url']}/{$this->records['model_url']}");
-                }
-                App::getSmarty()->assign('form', $this->form_rent);
-                App::getSmarty()->assign('user', SessionUtils::loadObject('user', true));
-                App::getSmarty()->assign('records', $this->records);
-            } catch (PDOException $ex) {
-                Utils::addErrorMessage('Wystąpił błąd podczas pobierania rekordów');
-                if (App::getConf()->debug) {
-                    Utils::addErrorMessage($ex->getMessage());
-                }
-            }
-        }
-
-        return !App::getMessages()->isError();
-    }
-
-    public function action_cars() {
-        $this->getParams();
 
         try {
             $brands = App::getDB()->select('car', '@brand');
@@ -189,7 +136,65 @@ class CarsCtrl {
             $this->records[$i]['model_url'] = preg_replace('/\s+/', '-', $this->records[$i]['model_url']);
         }
 //        print_r($this->records);
-        $this->generateView();
+        App::getSmarty()->assign('form', $this->form);
+        $this->assignSmarty();
+        return !App::getMessages()->isError();
+    }
+
+    public function processCar() {
+        $this->form_rent->id_car = $this->v->validateFromCleanURL(1, [
+            'required' => true,
+        ]);
+
+        if (!App::getMessages()->isError()) {
+            $where['id_car'] = $this->form_rent->id_car;
+
+            $brand = ParamUtils::getFromCleanURL(2);
+            $model = ParamUtils::getFromCleanURL(3);
+
+            $this->form_rent->rent_start = (new \DateTime('now', new \DateTimeZone('Europe/Warsaw')))->format('Y-m-d\TH:i');
+            $this->form_rent->rent_end = (new \DateTime('+1 day', new \DateTimeZone('Europe/Warsaw')))->format('Y-m-d\TH:i');
+
+            try {
+                $this->records = App::getDB()->get('car', [
+                    '[><]car_price' => 'id_car_price'
+                        ], '*', $where);
+
+                if (empty($this->records)) {
+                    Utils::addErrorMessage('Brak pojazdu o podanym ID!');
+                    SessionUtils::storeMessages();
+                    return false;
+                }
+
+                $this->records['brand_url'] = trim($this->records['brand']);
+                $this->records['model_url'] = trim($this->records['model']);
+                $this->records['brand_url'] = strtolower($this->records['brand_url']);
+                $this->records['model_url'] = strtolower($this->records['model_url']);
+                $this->records['brand_url'] = preg_replace('/\s+/', '-', $this->records['brand_url']);
+                $this->records['model_url'] = preg_replace('/\s+/', '-', $this->records['model_url']);
+
+                if ($brand !== $this->records['brand_url'] || $model !== $this->records['model_url']) {
+                    App::getRouter()->redirectTo("car/{$this->form_rent->id_car}/{$this->records['brand_url']}/{$this->records['model_url']}");
+                }
+                App::getSmarty()->assign('form', $this->form_rent);
+                $this->assignSmarty();
+            } catch (PDOException $ex) {
+                Utils::addErrorMessage('Wystąpił błąd podczas pobierania rekordów');
+                if (App::getConf()->debug) {
+                    Utils::addErrorMessage($ex->getMessage());
+                }
+            }
+        }
+
+        return !App::getMessages()->isError();
+    }
+
+    public function action_cars() {
+        if ($this->processCars()) {
+            App::getSmarty()->display('CarsListView.tpl');
+        } else {
+            App::getRouter()->redirectTo('main');
+        }
     }
 
     public function action_car() {
@@ -200,12 +205,9 @@ class CarsCtrl {
         }
     }
 
-    public function generateView() {
-        App::getSmarty()->assign('form', $this->form);
+    public function assignSmarty() {
         App::getSmarty()->assign('user', SessionUtils::loadObject('user', true));
         App::getSmarty()->assign('records', $this->records);
-
-        App::getSmarty()->display('CarsListView.tpl');
     }
 
 }
